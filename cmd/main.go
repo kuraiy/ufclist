c0fedebb2d3e59a1fbb2d885e7a6cd60aecbac77
kuraiy <beka1meilun@gmail.com>
2026-06-03 08:50:32 +0000

    env vars and starting server

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
+	"os"
 	"server/internal/handler"
 	"server/internal/repository/sqlite"
 	"server/internal/service"
+
+	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "fighters")
+	godotenv.Load()
+	dbPath := os.Getenv("DB_PATH")
+	port := os.Getenv("PORT")
+
+	db, err := sql.Open("sqlite3", dbPath)
 
 	if err != nil {
 		log.Fatal(err)
@@ -19,4 +28,13 @@ func main() {
 	repo := sqlite.New(db)
 	svc := service.New(repo)
 	handler := handler.New(svc)
+
+	mux := http.NewServeMux()
+	handler.RegisterRoutes(mux)
+
+	log.Printf("Listening on %s\n", port)
+	if err := http.ListenAndServe(port, mux); err != nil {
+		log.Fatal(err)
+	}
+
 }
