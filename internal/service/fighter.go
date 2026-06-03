6d6d22518934e57525242200d2e48bf965e4d3ea
kuraiy <beka1meilun@gmail.com>
2026-06-03 13:59:43 +0000

    delete unused

--- internal/service/fighter.go
+++ internal/service/fighter.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"errors"
 	"server/internal/domain"
 )
 
@@ -15,26 +14,10 @@ func New(repo domain.FighterRepository) *FighterService {
 }
 
 func (s *FighterService) Create(ctx context.Context, f domain.Fighter) (domain.Fighter, error) {
-	if f.Name == "" {
-		return domain.Fighter{}, errors.New("name is required")
-	}
-
-	if f.Age < 18 {
-		return domain.Fighter{}, errors.New("Age must be at least 18")
-	}
-
-	if f.Age > 100 {
-		return domain.Fighter{}, errors.New("Unsupported Age")
-	}
-
 	return s.repo.Create(ctx, f)
 }
 
 func (s *FighterService) GetByID(ctx context.Context, id int64) (domain.Fighter, error) {
-	if id < 0 {
-		return domain.Fighter{}, errors.New("Invalid id")
-	}
-
 	return s.repo.GetByID(ctx, id)
 }
 
