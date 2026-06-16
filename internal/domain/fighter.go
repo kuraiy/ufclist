2fc21181c436b87b5657ad29b595b0f740cca071
kuraiy <beka1meilun@gmail.com>
2026-06-16 12:39:07 +0000

    delete func interface

--- internal/domain/fighter.go
+++ internal/domain/fighter.go
@@ -1,6 +1,9 @@
 package domain
 
-import "context"
+import (
+	"context"
+	"database/sql"
+)
 
 type Fighter struct {
 	ID       int64
@@ -13,11 +16,12 @@ type FighterRepository interface {
 	Create(ctx context.Context, f Fighter) (Fighter, error)
 	GetByID(ctx context.Context, id int64) (Fighter, error)
 	List(ctx context.Context) ([]Fighter, error)
-	Delete(ctx context.Context, id int64) error
+	Delete(ctx context.Context, id int64) (sql.Result, error)
 }
 
 type FighterService interface {
 	Create(ctx context.Context, f Fighter) (Fighter, error)
 	GetByID(ctx context.Context, id int64) (Fighter, error)
 	List(ctx context.Context) ([]Fighter, error)
+	Delete(ctx context.Context, id int64) (struct{}, error)
 }
