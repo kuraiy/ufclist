95bf7409017338d95abf5ff8877d277b956b7a7f
kuraiy <beka1meilun@gmail.com>
2026-06-16 12:40:57 +0000

    delete route logic

--- internal/service/fighter.go
+++ internal/service/fighter.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"server/internal/domain"
 )
 
@@ -23,4 +25,19 @@ func (s *FighterService) GetByID(ctx context.Context, id int64) (domain.Fighter,
 
 func (s *FighterService) List(ctx context.Context) ([]domain.Fighter, error) {
 	return s.repo.List(ctx)
+}
+
+func (s *FighterService) Delete(ctx context.Context, id int64) (struct{}, error) {
+	result, err := s.repo.Delete(ctx, id)
+
+	if err != nil {
+		return struct{}{}, err
+	}
+
+	if rows, err := result.RowsAffected(); rows != 1 {
+		errMsg := fmt.Sprintf(`Fighter with id:%d is still activated, reason :%s`, id, err.Error())
+		return struct{}{}, errors.New(errMsg)
+	}
+
+	return struct{}{}, nil
 }
