0bfa82a06c0b6b9b65f2f1e719fb8f916cb74b39
kuraiy <beka1meilun@gmail.com>
2026-06-17 13:19:54 +0000

    moved struct to domain

--- internal/handler/dto.go
+++ internal/handler/dto.go
@@ -5,9 +5,3 @@ type CreateFighterRequest struct {
 	Age      int    `json:"age" validate:"required,gte=18,lte=100"`
 	Nickname string `json:"nickname" validate:"omitempty"`
 }
-
-type UpdateFighterRequest struct {
-	Name     string `json:"name" validate:"omitempty"`
-	Age      uint8  `json:"age" validate:"omitempty,min=18,max=100"`
-	Nickname string `json:"nickname" validate:"omitempty"`
-}
