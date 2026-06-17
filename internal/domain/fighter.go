82ce16097786b55c74297960d56e1930f8169649
kuraiy <beka1meilun@gmail.com>
2026-06-17 13:19:24 +0000

    add: struct for update method

--- internal/domain/fighter.go
+++ internal/domain/fighter.go
@@ -12,11 +12,19 @@ type Fighter struct {
 	Age      uint8
 }
 
+type UpdateFighterInput struct {
+	ID       int64
+	Name     *string
+	Age      *uint8
+	Nickname *string
+}
+
 type FighterRepository interface {
 	Create(ctx context.Context, f Fighter) (Fighter, error)
 	GetByID(ctx context.Context, id int64) (Fighter, error)
 	List(ctx context.Context) ([]Fighter, error)
 	Delete(ctx context.Context, id int64) (sql.Result, error)
+	Update(ctx context.Context, f Fighter) (Fighter, error)
 }
 
 type FighterService interface {
@@ -24,4 +32,5 @@ type FighterService interface {
 	GetByID(ctx context.Context, id int64) (Fighter, error)
 	List(ctx context.Context) ([]Fighter, error)
 	Delete(ctx context.Context, id int64) (struct{}, error)
+	Update(ctx context.Context, f UpdateFighterInput) (Fighter, error)
 }
