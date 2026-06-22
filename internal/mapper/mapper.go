2d671c23b3e01f1a1ec8692321b2709c244cdd72
kuraiy <beka1meilun@gmail.com>
2026-06-22 13:55:33 +0000

    upd: mappers for new responses

--- internal/mapper/mapper.go
+++ internal/mapper/mapper.go
@@ -5,11 +5,33 @@ import (
 	"server/internal/domain"
 )
 
-func MapRow(row database.Fighter) domain.Fighter {
+func MapFighter(row database.Fighter) domain.Fighter {
 	return domain.Fighter{
 		ID:       row.ID,
 		Name:     row.Name,
 		Age:      row.Age,
 		Nickname: row.Nickname.String,
 	}
+}
+
+func MapFighterRow(row database.GetFighterRow) domain.Fighter {
+	return domain.Fighter{
+		ID:       row.ID,
+		Name:     row.Name,
+		Age:      row.Age,
+		Nickname: row.Nickname.String,
+	}
+}
+
+func MapFighterRows(rows []database.ListFightersRow) []domain.Fighter {
+	out := make([]domain.Fighter, len(rows))
+	for i, r := range rows {
+		out[i] = domain.Fighter{
+			ID:       r.ID,
+			Name:     r.Name,
+			Age:      r.Age,
+			Nickname: r.Nickname.String,
+		}
+	}
+	return out
 }
